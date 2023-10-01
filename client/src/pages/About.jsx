09e5bb35751119129e825e004d2c31c65b59ba9f
Muhammed Zain <muhammedzain2256@gmail.com>
2023-10-01 20:45:54 +0500

    Changes

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+export default function About() {
+  return (
+    <div>About</div>
+  )
+}
---
+++ client/src/pages/SignIn.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+export default function SignIn() {
+  return (
+    <div>SignIn</div>
+  )
+}
