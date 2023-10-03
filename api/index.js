a100c519fc55a7fc0d7d0794a073cc0b12c694cc
Muhammed Zain <muhammedzain2256@gmail.com>
2023-10-03 10:24:14 +0500

    created a middleware

--- api/index.js
+++ api/index.js
@@ -22,6 +22,15 @@ app.listen(3000, () => {
     console.log('Server is running on port 3000');
 })
 
-
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({ 
+        success: false,
+        statusCode,
+        message,
+    });
+})
