1d422ec2dd7a543a7bce6ca7bc2721c57b8e2faf
Muhammed Zain <muhammedzain2256@gmail.com>
2023-10-04 08:04:29 +0500

    Added Redux Toolkit

---
+++ client/src/redux/user/userSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    currentUser: null,
+    error: null,
+    loading: false,
+}
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        siginInStart: (state) => {
+            state.loading = true;
+        },
+        signInSuccess: (state, action) => {
+            state.currentUser = action.payload;
+            state.loading = false;
+            state.error = null;
+        },
+        signInFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+        }
+    }
+});
+
+export const { siginInStart, signInSuccess, signInFailure } = userSlice.actions;
+
+export default userSlice.reducer;
