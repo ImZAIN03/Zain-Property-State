2c00653ac61de82842f67b991ccc06887afcbef6
Muhammed Zain <muhammedzain2256@gmail.com>
2023-10-07 22:12:53 +0500

    Completed Listing Page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -19,6 +19,7 @@ import {
   signOutUserStart
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 export default function Profile() {
   const fileRef = useRef(null)
   const { currentUser, loading, error } = useSelector(state => state.user)
@@ -179,7 +180,9 @@ export default function Profile() {
         <button disabled={loading} className='bg-gray-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>
           {loading ? 'loading...': 'Update'}
           </button>
-      
+          <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95' to={'/create-listing'}>
+            Create Listing
+          </Link>
       </form>
       <div className='flex justify-between mt-5'>
         <span onClick={handleDeleteUser} className='text-red-600 cursor-pointer'>Delete Account</span>
